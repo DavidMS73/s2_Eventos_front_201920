c19e8ae549ba9b4cbaf26a951eb80e025ff13363
Daniel Santiago Tenjo Leal <ds.tenjo@uniandes.edu.co>
2019-11-11 22:07:02 -0500

    ahora si

--- src/app/medioDePago/medioDePago.module.ts
+++ src/app/medioDePago/medioDePago.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 import { MedioDePagoService } from './medioDePago.service';
 import { MedioDePagoListComponent } from './medioDePago-list/medioDePago-list.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [MedioDePagoListComponent],
   providers: [MedioDePagoService],
