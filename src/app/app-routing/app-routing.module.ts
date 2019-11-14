c3d50b55d01421264bc95003d180a9a3317396f8
Estudiante <Estudiante@ISIS2603-118.sis.virtual.uniandes.edu.co>
2019-11-13 23:53:46 -0500

    Mejora de la interfaz

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -41,11 +41,7 @@ const routes: Routes = [
                 }
             }
         ]
-    },
-    {
-        path: 'home',
-        component: AuthLoginComponent
-    },
+    }, 
     {
         path: 'memorias',
         children: [
